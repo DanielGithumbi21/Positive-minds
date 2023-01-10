f0ef486e987f3655c25b2392fedda1306248982b
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-01-10 14:50:18 +0300

    Creating login and signup screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 import RoutesPage from './Routes/RoutesPage'
 import { Route, Routes } from 'react-router'
 import LandingPage from './components/LandingPage/LandingPage'
-
+import Login from './components/Sign/Login'
+import "bootstrap/dist/css/bootstrap.min.css"
+import SignUp from './components/Sign/SignUp'
 const App = () => {
   return (
 
     <React.Fragment>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <RoutesPage />
     </React.Fragment>
