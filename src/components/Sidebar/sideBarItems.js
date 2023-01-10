407536f7532d3825c28f87ecf9efaaf765a5fd7a
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-01-10 08:48:11 +0300

    created the sidebar boilerplate

---
+++ src/components/Sidebar/sideBarItems.js
@@ -0,0 +1,61 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import EmailIcon from "@mui/icons-material/Email";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import PersonIcon from "@mui/icons-material/Person";
+import BookOnlineIcon from "@mui/icons-material/BookOnline";
+import HourglassFullIcon from "@mui/icons-material/HourglassFull";
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+const drawerItems = [
+  {
+    name: "",
+    items: [
+      {
+        name: "Dashboard",
+        Icon: DashboardIcon,
+        link: "/",
+      },
+    ],
+  },
+  {
+    name: "Orders",
+    items: [
+      {
+        name: "View Available Sessions",
+        Icon: EmailIcon,
+        link: "/",
+      },
+      {
+        name: "Calendar",
+        Icon: CalendarMonthIcon,
+        link: "/",
+      },
+      {
+        name: "Become a Therapist",
+        Icon: PersonIcon,
+        link: "/",
+      },
+      {
+        name: "Book a Private Session",
+        Icon: BookOnlineIcon,
+        link: "/",
+      },
+    ],
+  },
+
+  {
+    name: "Account",
+    items: [
+      {
+        name: "My Profile",
+        Icon: HourglassFullIcon,
+        link: "/",
+      },
+      {
+        name: "Logout",
+        Icon: PowerSettingsNewIcon,
+        link: "/",
+      },
+    ],
+  },
+];
+export { drawerItems };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { DataProvider } from './Context/DataContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <DataProvider>
     <App />
-  </React.StrictMode>,
+  </DataProvider>,
 )
