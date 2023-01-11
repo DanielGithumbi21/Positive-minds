24b3194a9e8136642d252ae870397fc4e0751f27
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-01-11 13:37:44 +0300

    integrated login/signup with the backend

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,7 +5,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { DataContext } from '../../Context/DataContext';
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 300;
 const AppBar = styled(MuiAppBar, {
@@ -25,7 +30,23 @@ const AppBar = styled(MuiAppBar, {
     }),
 }));
 const TopAppBar = () => {
-    const { open, handleDrawerOpen } = useContext(DataContext)
+    const navigate = useNavigate()
+    const { open, handleDrawerOpen, user, setLoggedUser } = useContext(DataContext)
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const checkOpen = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+
+    };
+    const logout = () => {
+        localStorage.removeItem("user")
+        setLoggedUser(false)
+        navigate("/login")
+    }
 
 
     return (
@@ -45,16 +66,35 @@ const TopAppBar = () => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <div className="row padding">
-                        <div className="col-lg-6">
+                    <Button
+                        id="basic-button"
+                        aria-controls={checkOpen ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={checkOpen ? 'true' : undefined}
+                        onClick={handleClick}
 
-                        </div>
-                        <div className="col-lg-6">
-                            <Typography variant="h6" noWrap component="div">
-                                Welcome, Moraa
-                            </Typography>
-                        </div>
-                    </div>
+                    >
+                        <PowerSettingsNewIcon sx={{
+                            color: "#808080"
+                        }} />
+                    </Button>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={checkOpen}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>
+                            <button className="btn btn-outline btn-md" onClick={logout}>Logout</button>
+                        </MenuItem>
+                    </Menu>
+
+                    <Typography variant="h6" noWrap component="div" >
+                        Welcome, {user.user.name}
+                    </Typography>
 
                 </Toolbar>
             </AppBar>
--- src/components/Sign/Login.jsx
+++ src/components/Sign/Login.jsx
@@ -1,50 +1,203 @@
-import React, { useState } from 'react'
-import { Typography, TextField, Container, Card } from '@mui/material';
+import React, { useContext, useState } from 'react'
+import { Typography, TextField, Container, Card, MenuItem } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+
 import { useNavigate } from 'react-router';
+import { loginUser, registerUser } from '../../Data/Auth/Data';
+import { DataContext } from '../../Context/DataContext';
 
 const Login = () => {
-    const [isCounsellor, setIsCounsellor] = useState(false)
-    const switchMode = () => setIsCounsellor((prev) => !prev)
+    const [isSignup, setIsSignup] = useState(false)
+    const switchMode = () => setIsSignup((prev) => !prev)
     const navigate = useNavigate()
+    const initialState = { name: "", username: "", email: "", gender: "", age: "", avatar: "", password: "" }
+    const [formData, setFormData] = useState(initialState)
+    const { setUser, setLoggedUser } = useContext(DataContext)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+        setError("")
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        isSignup ?
+            registerUser(formData, setLoggedUser, setUser, navigate, setIsLoading, setError)
+            : loginUser(formData, setLoggedUser, setUser, navigate, setIsLoading, setError)
+    }
+    console.log(formData)
+
     return (
         <Container sx={{
             width: "40%",
-            marginTop: "10%"
+            marginTop: "9%"
         }}>
             <Card sx={{
                 textAlign: "center",
-                padding: "20px"
+                padding: "20px",
+                backgroundColor: "#FFF"
             }} >
                 <Typography variant="h5" className='mb-3' gutterBottom>
                     Positive Minds
                 </Typography>
                 <Typography variant="subtitle1" className='mb-5' gutterBottom>
-                    Please sign in as a {isCounsellor ? "Counsellor" : "Patient"}.
+                    Please {isSignup ? "Register" : "Login"}  .
                 </Typography>
-                <div class="form-floating mb-3">
-                    <TextField
-                        fullWidth
-                        id="outlined-required"
-                        label="Email"
-                        type="email"
-                    />
-                </div>
-                <div class="form-floating mb-3">
-                    <TextField
-                        fullWidth
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                    />
 
-                </div>
-                <button className="btn btn-primary btn-md mb-3">Login</button>
+                {
+                    isSignup ?
+                        <form onSubmit={onSubmit}>
+                            <Typography variant="subtitle1" className='mb-4' gutterBottom>
+                                {error}
+                            </Typography>
+                            <div className="row padding">
+
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <TextField
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                            id="outlined-required"
+                                            label="Name"
+                                            type="text"
+                                            name="name"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <TextField
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                            id="outlined-required"
+                                            label="Username"
+                                            type="text"
+                                            name="username"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <TextField
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                            id="outlined-required"
+                                            label="Email"
+                                            type="email"
+                                            name="email"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                value={formData.gender}
+                                                label="Gender"
+                                                onChange={handleChange}
+                                                required
+                                                name="gender"
+                                            >
+                                                <MenuItem value="Male">Male</MenuItem>
+                                                <MenuItem value="Female">Female</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <TextField
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                            id="outlined-required"
+                                            label="Age"
+                                            type="text"
+                                            name="age"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div class="form-floating mb-3">
+                                        <TextField
+                                            onChange={handleChange}
+                                            required
+                                            fullWidth
+                                            id="outlined-required"
+                                            label="Avatar"
+                                            type="text"
+                                            name="avatar"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <TextField
+                                        onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        id="outlined-password-input"
+                                        label="Password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        name='password'
+                                    />
+
+                                </div>
+
+
+                            </div>
+                            <button className="btn btn-primary btn-md mb-3" disabled={isLoading ? true : false} >  Register</button>
+
+                        </form>
+                        :
+                        <React.Fragment>
+                            <form onSubmit={onSubmit}>
+                                <Typography variant="subtitle1" className='mb-4 text-danger' gutterBottom>
+                                    {error}
+                                </Typography>
+                                <div class="form-floating mb-3">
+                                    <TextField
+                                        onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        id="outlined-required"
+                                        label="Username"
+                                        type="text"
+                                        name='username'
+                                    />
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <TextField
+                                        onChange={handleChange}
+                                        required
+                                        fullWidth
+                                        id="outlined-password-input"
+                                        label="Password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        name="password"
+                                    />
+
+                                </div>
+                                <button className="btn btn-primary btn-md mb-3" disabled={isLoading ? true : false}>  Login</button>
+
+                            </form>
+                        </React.Fragment>
+                }
+
+
                 <div>
-                    <button className="btn btn-outline-primary btn-md mb-3" onClick={switchMode}>Login as a {isCounsellor ? "Patient" : "Counsellor"} </button>
-                </div>
-                <div>
-                    Dont have an account? <button className='btn btn-sm btn-outline ' onClick={(() => navigate("/sign-up"))} >Register</button>
+                    <button className="btn btn-outline-primary btn-md mb-3" onClick={switchMode}>{isSignup ? "Already have an account? Login" : "Don't have an account? Register"} </button>
                 </div>
 
             </Card>
