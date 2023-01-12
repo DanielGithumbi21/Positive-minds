ac2a406ce569858ef144cfc2c39424a9c61d9842
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2023-01-12 09:19:45 +0300

    added an admin route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import RoutesPage from './Routes/RoutesPage'
 import { Route, Routes } from 'react-router'
 import LandingPage from './components/LandingPage/LandingPage'
 import Login from './components/Sign/Login'
+import Admin from './components/Admin/AdminDashboard'
 import "bootstrap/dist/css/bootstrap.min.css"
+import AdminDashboard from './components/Admin/AdminDashboard'
 const App = () => {
   return (
 
@@ -12,6 +14,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard/>} />
       </Routes>
       <RoutesPage />
     </React.Fragment>
