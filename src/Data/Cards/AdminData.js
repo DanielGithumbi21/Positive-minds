1c6ae6f00db4ecf8aa8cbe49b3936f10bd39a327
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2023-01-12 23:47:34 +0300

    progress on my card components

---
+++ src/Data/Cards/AdminData.js
@@ -0,0 +1,42 @@
+import { color } from "@mui/system";
+
+// cards data
+export const CardsData =[
+    {
+        title: "Sessions",
+        color: {
+            backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%',
+            boxShadow: "0px 10px 20px 0px #e0c6f5",
+        },
+        barValue: 45,
+        value: "12",
+    
+    },
+    {
+        title: "Appointments",
+        color: {
+            backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%',
+            boxShadow: "0px 10px 20px 0px #e0c6f5",
+        },
+        barValue: 70,
+        value:160,
+    },
+    {
+        title: "Patients",
+        color: {
+            backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%',
+            boxShadow: "0px 10px 20px 0px #e0c6f5",
+        },
+        barValue: 70,
+        value:160,
+    },
+    {
+        title: "Counselors",
+        color: {
+            backGround: 'linear-gradient(180deg, #bb67ff 0%, #c484f3 100%',
+            boxShadow: "0px 10px 20px 0px #e0c6f5",
+        },
+        barValue: 30,
+        value:50,
+    },
+]
--- src/components/Admin/AdminDashboard.jsx
+++ src/components/Admin/AdminDashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './AdminDashbord.css'
+import MainDash from '../MainDash/MainDash'
 function AdminDashboard() {
   return (
     <div className='AdminDashboard'>
       <div className='AdminGlass'>
+        <MainDash/>
       </div>
     </div>
   )
