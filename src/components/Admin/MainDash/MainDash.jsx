7dd32a81222a624e80a7f2bb0374765d6e330896
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2023-01-17 14:08:20 +0300

    Progress

--- src/components/Admin/MainDash/MainDash.jsx
+++ src/components/Admin/MainDash/MainDash.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './MainDash.css'
-import Cards from '../Cards/Cards'
+import Cards from '../CardsAdmin/CardsAdmin'
+import Table from '../Table/Table'
 function MainDash() {
   return (
     <div className='MainDash'>
       <h1>DashBoard</h1>
       <Cards/>
+      <Table/>
     </div>
   )
 }
