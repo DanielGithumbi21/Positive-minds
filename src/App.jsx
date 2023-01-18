9189915992caca7bdb4e11ec1d85defe307d9e27
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-01-18 10:23:25 +0300

    counselor profile requests

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin/>} />
       </Routes>
       <RoutesPage />
     </React.Fragment>
