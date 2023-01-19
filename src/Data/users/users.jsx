ed503a3719a71a29e6563808f93ecd7024e3f93b
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2023-01-19 06:57:15 +0300

    Style edit profile form. Post data.

--- src/Data/users/users.jsx
+++ src/Data/users/users.jsx
@@ -2,7 +2,7 @@ import { api } from "../Auth/Data";
 import authHeader from "../Auth/AuthHelper";
 import axios from "axios";
 
-const getProfile = async () => {
+const getProfile = async (setIsLoading) => {
     try {
         setIsLoading(true);
         const response = await fetch(`${api}/profile`, {
@@ -88,6 +88,19 @@ const bookAppointment = async (post, setIsLoading, navigate) => {
     }
 }
 
+const updateProfile = async (formData, userId) => {
+    try {
+        const response = await axios.put(`${api}/users/${userId}`, formData, {
+            headers: authHeader(),
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+
 export {
-    getProfile, beTherapist, getCounselorProfile, bookAppointment, getSingleCounselorProfile, deleteCounsellorProfile, verifyCounsellor
+    getProfile, beTherapist, getCounselorProfile, bookAppointment, getSingleCounselorProfile, deleteCounsellorProfile, verifyCounsellor, updateProfile
 }
